48e35acac611384668dd1879d0b3825a2a34b3a3
Daniel Diniz <daniel.diniz1@icloud.com>
2022-03-21 20:20:29 -0300

    substituindo herança por composição

---
+++ src/br/com/alura/rh/model/Terceirizado.java
@@ -0,0 +1,14 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+
+public class Terceirizado {
+
+    private DadosPessoais dadosPessoais;
+    private String empresa;
+
+    public Terceirizado(DadosPessoais dadosPessoais, String empresa) {
+        this.dadosPessoais = dadosPessoais;
+        this.empresa = empresa;
+    }
+}
